6c199297e174df88a8da4a365350479343cece1d
Yurii <www.yura.klek@gmail.com>
2019-01-10 20:11:59 +0200

    start of project

---
+++ ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykliek <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/10 19:55:14 by ykliek            #+#    #+#             */
+/*   Updated: 2019/01/10 19:55:15 by ykliek           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h> // delete before validation
+# include <stdarg.h>
+# include "./libft/libft.h"
+
+#endif
